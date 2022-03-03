45ef8c7eee85964d2ff52bc43f247b47ee9471ed
ericamcg <71267518+erica-kane@users.noreply.github.com>
2022-03-03 11:43:30 +0000

    collect up to january

--- collection.R
+++ collection.R
@@ -1,10 +1,11 @@
 library(academictwitteR)
+library(tidyverse)
 
 # US tweets from 26 May - 31 August
 #get_all_tweets(
 #  users = c("metpoliceuk", "gmpolice", "ASPolice", "WestYorksPolice", 'WMPolice'),
 #  start_tweets = "2019-01-01T00:00:00Z",
-#  end_tweets = "2021-11-01T00:00:00Z",
+#  end_tweets = "2022-01-01T00:00:00Z",
 #  file = "tweets.rds",
 #  n = 100000)
 
@@ -21,9 +22,5 @@ all_tweets3 = all_tweets2 %>%
 tweets = all_tweets3[, c('id', 'author_id', 'created_at', 'text', 'in_reply_to_user_id', 
                          'retweet_count', 'reply_count', 'like_count', 'quote_count')]
 
-# 'mentions', 'hashtags', 'urls' were going to be extracted but it didn't work
-# tweets$mentions[[1]][[3]]
-# tweets$hashtags[[2]][[3]]
-
-write_csv(tweets, 'tweets.csv')
+write_csv(tweets, 'tweets1.csv')
 
