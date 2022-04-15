bb342cd252891328445ca4717b01e212c6826523
ericamcg <71267518+erica-kane@users.noreply.github.com>
2022-04-15 17:19:49 +0100

    plots

--- plots.R
+++ plots.R
@@ -12,6 +12,7 @@ plot_tweets$date = as.Date(plot_tweets$date)
 
 plot_tweets$tweet_class = recode(plot_tweets$tweet_class, '1' = "Pushing information", '2' = "Engagement", '3' = "Intelligence gathering")
 
+# Plot for Class per force 
 ggplot(plot_tweets, aes(police_force)) + 
   geom_bar(stat = 'count', aes(fill = tweet_class), position = 'fill', color = 'grey') +
   scale_fill_brewer(name = 'Tweet class') +
@@ -19,34 +20,33 @@ ggplot(plot_tweets, aes(police_force)) +
   theme(axis.text.x = element_text(angle = 45, vjust = 0.65)) +
   labs(x = 'Police force', y = 'Percentage relative to force count') 
 
+
+# Plots for use of classes per force over time 
+# Line graph per week
 plot_tweets %>%
   mutate(week=floor_date(date, unit="week")) %>%
   group_by(week, police_force, tweet_class) %>%
   summarise(Count = n()) %>%
   ggplot(aes(x = week, y = Count, color = tweet_class)) +
-  geom_line() + 
-  facet_wrap(~ police_force, ncol = 1) +
+  scale_color_manual(values=c("lightskyblue1", 'blue4',"dodgerblue1")) +
+  geom_line(size = 0.3) + 
+  facet_wrap(~ police_force, ncol = 1, scales="free") +
   labs(x = 'Date', y = 'Tweet count (aggregated by week)', title = 'Police force tweet frequency per class', color = 'Tweet class') +
   theme_minimal()
 
+# Line graph per month
 plot_tweets %>%
-  mutate(week=floor_date(date, unit="week")) %>%
-  group_by(week, police_force, tweet_class) %>%
+  mutate(month=floor_date(date, unit="month")) %>%
+  group_by(month, police_force, tweet_class) %>%
   summarise(Count = n()) %>%
-  ggplot(aes(x=week, fill=tweet_class)) +
-  geom_histogram(position="identity", alpha=0.2, bins = 50) +
-  facet_wrap(~ police_force, ncol = 1) +
-  labs(x = 'Date', y = 'Tweet count (aggregated by week)', title = 'Police force tweet frequency per class', color = 'Tweet class') +
+  ggplot(aes(x = month, y = Count, color = tweet_class)) +
+  scale_color_manual(values=c("dodgerblue1", 'gold', 'firebrick1')) +
+  geom_line(size = 0.3) + 
+  facet_wrap(~ police_force, ncol = 1, scales="free") +
+  labs(x = 'Date', y = 'Tweet count (aggregated by month)', title = 'The use of classes per police force over time', color = 'Tweet class') +
   theme_minimal()
-  
-plot_tweets %>%
-  mutate(week=floor_date(date, unit="week")) %>%
-  group_by(week, police_force, tweet_class) %>%
-  summarise(Count = n()) %>%
-  ggplot(aes(x=week)) + 
-  geom_density(aes(fill=tweet_class), alpha = 0.2)+ 
-  facet_wrap( ~ police_force, ncol = 1)+ 
-  theme_minimal()
+
+# 
 
 
 
--- total_tweet_plot.R
+++ total_tweet_plot.R
@@ -13,30 +13,6 @@ plot_tweets %>%
   mutate(week=floor_date(date, unit="week")) %>%
   group_by(week, police_force) %>%
   summarise(Count = n()) %>%
-  ggplot(aes(x = week, y = Count, color = police_force)) + 
-  geom_line(aes(color = police_force), size = 0.2, alpha = 0.6) +
-  scale_color_manual(values = c('cadetblue3', 'blue', 'deepskyblue', 'dodgerblue', 'dodgerblue4')) +
-  #geom_smooth(size = 0.5, span = 0.5) +
-  geom_vline(xintercept=as.numeric(as.Date('2020-05-25'))) +
-  theme_minimal() 
-
-plot_tweets %>%
-  mutate(week=floor_date(date, unit="week")) %>%
-  group_by(week, police_force) %>%
-  summarise(Count = n()) %>%
-  ggplot(aes(x = week, y = Count)) +
-  facet_grid(rows = vars(police_force)) +
-  geom_line(size = 0.4, alpha = 0.5) +
-  #scale_color_manual(values = c('cadetblue3', 'blue', 'deepskyblue', 'dodgerblue', 'dodgerblue4')) +
-  #geom_smooth(size = 0.2, color = 'gray22', alpha = 0.1) +
-  geom_vline(xintercept=as.numeric(as.Date('2020-05-25')), alpha = 0.4, size = 0.2, color = 'gray22', linetype = 'dashed') +
-  labs(x = 'Date', y = 'Tweet count (aggregated by week)', title = 'Police force tweet frequency', color = 'Police force') +
-  theme_minimal() 
-
-plot_tweets %>%
-  mutate(week=floor_date(date, unit="week")) %>%
-  group_by(week, police_force) %>%
-  summarise(Count = n()) %>%
   ggplot(aes(x = week, y = Count, color = police_force)) +
   facet_wrap(~police_force, ncol = 1) +
   geom_line(size = 0.4, alpha = 0.5) +
@@ -46,6 +22,24 @@ plot_tweets %>%
   labs(x = 'Date', y = 'Tweet count (aggregated by week)', title = 'Police force tweet frequency', color = 'Police force') +
   theme_minimal() +
   theme(legend.position = 'none')
+
+
+plot_tweets %>%
+  mutate(week=floor_date(date, unit="week")) %>%
+  group_by(week, police_force) %>%
+  summarise(Count = n()) %>%
+  ggplot(aes(x = week, y = Count, color = police_force)) +
+  facet_wrap(~police_force, ncol = 1) +
+  geom_point(size = 0.4, alpha = 0.5) +
+  geom_smooth(size = 0.2, color = 'gray88', alpha = 0.1) +
+  scale_color_manual(values = c('cadetblue3', 'blue', 'deepskyblue', 'dodgerblue', 'dodgerblue4')) +
+  geom_vline(xintercept=as.numeric(as.Date('2020-05-25')), alpha = 0.4, size = 0.2, color = 'gray22', linetype = 'dashed') +
+  labs(x = 'Date', y = 'Tweet count (aggregated by week)', title = 'Police force tweet frequency', color = 'Police force') +
+  theme_minimal() +
+  theme(legend.position = 'none')
+
+
+
 
 
 
