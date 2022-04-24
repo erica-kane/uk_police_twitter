c1ac245798f6f62d217681d299c76bc5846bb7fe
ericamcg <71267518+erica-kane@users.noreply.github.com>
2022-04-24 17:28:52 +0100

    adjust naive bayes

--- analysis.py
+++ analysis.py
@@ -20,6 +20,10 @@ all_tweets = all_tweets.drop(['Unnamed: 0'], axis=1)
 tweets_with_labels = all_tweets[all_tweets['tweet_class']!= 0][all_tweets['tweet_form'] == 'non duplicate']
 tweets_with_labels = tweets_with_labels[tweets_with_labels['tweet_class']!=100.0]
 
+# Adjust so that the sample is less skewed to class 1
+tweets_with_labels['tweet_class'].value_counts()
+tweets_with_labels = tweets_with_labels.drop(tweets_with_labels[tweets_with_labels['tweet_class'] == 1.0].sample(n=3242).index)
+tweets_with_labels = tweets_with_labels.drop(tweets_with_labels[tweets_with_labels['tweet_class'] == 3.0].sample(n=287).index)
 
 # Train test split
 train, test = train_test_split(tweets_with_labels)
@@ -40,18 +44,18 @@ accuracy_score(y_test, predicted_categories)
 
 class_names = ['Providing information','Engagement', 'Intelligence gathering']
 
-mat = confusion_matrix(y_test, predicted_categories_)
+mat = confusion_matrix(y_test, predicted_categories)
 sns.heatmap(mat.T, square = True, annot=True, fmt = "d", xticklabels = class_names, yticklabels = class_names)
 plt.xlabel("true labels")
 plt.ylabel("predicted label")
 plt.show()
 
-# First drop tweets with 100, then create 5 separate classifiers per force
-# Makes more sense to classify based on similar styles 
-
+# Save test tweets 
 test_with_base = test[['base_text','tweet_class']]
 test_with_base['pred_class'] = predicted_categories
-test_with_base[test_with_base['pred_class']!= test_with_base['tweet_class']]
+test_with_base.to_csv('test_labels.csv')
+# Save tweets which are classified incorrectly 
+test_with_base[test_with_base['pred_class']!= test_with_base['tweet_class']].to_csv('incorrect_labels.csv')
 
 
 
