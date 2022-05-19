59ef1df302ca30af8837f5659423b518100a84a3
ericamcg <71267518+erica-kane@users.noreply.github.com>
2022-05-19 16:31:06 +0100

    Update classification.py

--- classification.py
+++ classification.py
@@ -10,11 +10,14 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.pipeline import make_pipeline
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
 from sklearn.linear_model import LogisticRegression
 import seaborn as sns
 import matplotlib.pyplot as plt
 import eli5
+from sklearn.preprocessing import FunctionTransformer
+from sentence_transformers import SentenceTransformer
+from xgboost import XGBClassifier
 
 # Read the data 
 all_tweets = pd.read_csv('pre_pro_tweets.csv')
@@ -46,33 +49,24 @@ train, test = train_test_split(tweets_with_labels)
 # You need both features and labels for the training and testing to be separate 
 x_train = train['base_text']
 x_test = test['base_text']
+
+# Create x without stop words
+x_train_nsw = train['pre_pro_tweet']
+x_test_nsw = test['pre_pro_tweet']
+
+# Create y
 y_train = train['tweet_class']
 y_test = test['tweet_class']
 
-# Make Naive Bayes model
-vectorizer = TfidfVectorizer()
-classifier_nb = MultinomialNB()
-model_nb = make_pipeline(vectorizer, classifier_nb)
-model_nb.fit(x_train, y_train)
-predicted_categories_nb = model_nb.predict(x_test)
-accuracy_score(y_test, predicted_categories_nb)
 
-# Save test tweets for NB confusion matrix
-test_with_base_nb = test[['base_text','tweet_class']]
-test_with_base_nb['pred_class'] = predicted_categories_nb
-test_with_base_nb.to_csv('nb_test_labels.csv')
-# Save tweets which are classified incorrectly 
-test_with_base_nb[test_with_base_nb['pred_class']!= test_with_base_nb['tweet_class']].to_csv('nb_incorrect_labels.csv')
-
-# Logistic Regression 
+# Logistic Regression - with tfidf (88 with stop words, 85 without)
 classifier_logit = LogisticRegression()
 model_logit = make_pipeline(vectorizer, classifier_logit)
 model_logit.fit(x_train, y_train)
 predicted_categories_logit = model_logit.predict(x_test)
 accuracy_score(y_test, predicted_categories_logit)
-eli5.show_weights(classifier_logit, vec=vectorizer, top=10)
 
-# Save test tweets for NB confusion matrix
+# Save test tweets for logit confusion matrix
 test_with_base_logit = test[['base_text','tweet_class']]
 test_with_base_logit['pred_class'] = predicted_categories_logit
 test_with_base_logit.to_csv('logit_test_labels.csv')
@@ -80,11 +74,18 @@ test_with_base_logit.to_csv('logit_test_labels.csv')
 test_with_base_logit[test_with_base_logit['pred_class']!= test_with_base_logit['tweet_class']].to_csv('logit_incorrect_labels.csv')
 
 
+# Naive Bayes model - with tfidf (85 with stop words, 85 without)
+vectorizer = TfidfVectorizer()
+classifier_nb = MultinomialNB()
+model_nb = make_pipeline(vectorizer, classifier_nb)
+model_nb.fit(x_train, y_train)
+predicted_categories_nb = model_nb.predict(x_test)
+accuracy_score(y_test, predicted_categories_nb)
+precision_score(y_test, predicted_categories_nb, average='micro')
+recall_score(y_test, predicted_categories_nb, average='micro')
 
 
-# SentenceTransformer - 84%
-from sklearn.preprocessing import FunctionTransformer
-from sentence_transformers import SentenceTransformer
+# Logistic regression - with embeddings (85 with stop words, 84 without)
 senttrans = SentenceTransformer('all-MiniLM-L6-v2')
 embedder = FunctionTransformer(lambda item: senttrans.encode(item, convert_to_numpy=True, show_progress_bar=False))
 model_senttrans_logit = make_pipeline(embedder, LogisticRegression())
@@ -92,14 +93,15 @@ model_senttrans_logit.fit(list(x_train), y_train)
 pred_cat_senttrans_logit = model_senttrans_logit.predict(list(x_test))
 accuracy_score(y_test, pred_cat_senttrans_logit)
 
-# XGBoost - 83%
-from xgboost import XGBClassifier
+# XGBoost - with embeddings (85 with stop words, 84 without)
 embedder = FunctionTransformer(lambda item: senttrans.encode(item, convert_to_numpy=True, show_progress_bar=False))
 model_senttrans_xgb = make_pipeline(embedder, XGBClassifier(objective='multi:softmax'))
 model_senttrans_xgb.fit(list(x_train), (y_train - 1).astype(int))
 pred_cat_senttrans_xgb = model_senttrans_xgb.predict(list(x_test))
 accuracy_score((y_test - 1).astype(int), pred_cat_senttrans_xgb)
 
+
+# Optional plotting 
 class_names = ['Providing information','Engagement', 'Intelligence gathering']
 mat = confusion_matrix(y_test, predicted_categories)
 sns.heatmap(mat.T, square = True, annot=True, fmt = "d", xticklabels = class_names, yticklabels = class_names)
